6eecd27b6cd1188cd9129500eec87d2ff82b27c9
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-01-31 10:52:53 +0100

    Ajout debut save

---
+++ Classes/Save.cs
@@ -0,0 +1,24 @@
+﻿using Project_Easy_Save.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Easy_Save.Classes
+{
+    internal class Save
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SourcePath { get; set; }
+        public string DestinationPath { get; set; }
+        public SaveType Type { get; set; }
+        public DateTime LastExecuteDate { get; set; }
+
+        public void Execute()
+        {
+
+        }
+    }
+}
