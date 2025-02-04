01a20fdd89ec78af6bb72b14857681994e34eda2
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-02-04 11:47:01 +0100

    start from edit save,

--- Classes/EditSavesPrompt.cs
+++ Classes/EditSavesPrompt.cs
@@ -43,7 +43,7 @@ namespace Project_Easy_Save.Classes
                         break;
 
 					case '5':
-						Quitter();
+						Exit();
 						break;
 				}
 			}
@@ -63,20 +63,62 @@ namespace Project_Easy_Save.Classes
 			Console.WriteLine("Appuyez sur une touche pour continuer\n\n");
 		}
 
-		private void CreateSave()
-		{
+        private void CreateSave()
+        {
             Console.Write("Choisissez un nom pour la sauvegarde : ");
             string Name = Console.ReadLine();
-            Console.Write("Choisissez un type pour la sauvegarde : ");
-            SaveType Type = (SaveType)Enum.Parse(typeof(SaveType), Console.ReadLine());
-            Console.Write("Choisissez un chemin source pour la sauvegarde : ");
-            string SourcePath = Console.ReadLine();
-            Console.Write("Choisissez un chemin destination pour la sauvegarde : ");
-            string DestinationPath = Console.ReadLine();
+
+            SaveType Type;
+            while (true)
+            {
+                Console.Write("Choisissez un type pour la sauvegarde (1: Full, 2: Differential) : ");
+                string typeInput = Console.ReadLine();
+                if (typeInput == "1")
+                {
+                    Type = SaveType.Full;
+                    break;
+                }
+                else if (typeInput == "2")
+                {
+                    Type = SaveType.Differential;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Type de sauvegarde invalide. Veuillez entrer 1 pour Full ou 2 pour Differential.");
+                }
+            }
+
+            string SourcePath;
+            do
+            {
+                Console.Write("Choisissez un chemin source pour la sauvegarde : ");
+                SourcePath = Console.ReadLine();
+                if (!Directory.Exists(SourcePath))
+                {
+                    Console.WriteLine("Le chemin source n'existe pas. Veuillez entrer un chemin valide.");
+                }
+            } while (!Directory.Exists(SourcePath));
+
+            string DestinationPath;
+            do
+            {
+                Console.Write("Choisissez un chemin destination pour la sauvegarde : ");
+                DestinationPath = Console.ReadLine();
+                if (!Directory.Exists(DestinationPath))
+                {
+                    Console.WriteLine("Le chemin destination n'existe pas. Veuillez entrer un chemin valide.");
+                }
+            } while (!Directory.Exists(DestinationPath));
+
             _saveStore.CreateNewSave(Name, Type, SourcePath, DestinationPath);
+            Console.Clear();
+            Console.WriteLine("La sauvegarde a été créée.");
         }
 
-		private void DisplaySave()
+
+
+        private void DisplaySave()
 		{
 			_saveStore.DisplayAllSaves();
             Console.WriteLine("Choisissez une savegarde à afficher : ");
@@ -85,18 +127,75 @@ namespace Project_Easy_Save.Classes
 
 		private void EditerSauvegarde()
 		{
-			Console.Clear();
-			Console.WriteLine("Édition d'une sauvegarde");
-		}
+            string newValue = string.Empty;
+            _saveStore.DisplayAllSaves();
+            Console.WriteLine("Choisissez une sauvegarde à éditer : ");
+            int id = int.Parse(Console.ReadLine());
+            _saveStore.DisplaySave(id);
+
+            string property;
+            while (true)
+            {
+                Console.WriteLine("Quelle propriété voulez-vous éditer ?");
+                Console.WriteLine("1. Nom, 2. Type, 3. sourcePath, 4. destinationPath");
+                string typeInput = Console.ReadLine();
+                if (typeInput == "1")
+                {
+                    property = "Name";
+
+                    break;
+                }
+                else if (typeInput == "2")
+                {
+                    property  = "Type";
+                    break;
+                }
+                else if (typeInput == "3")
+                {
+                    property = "sourcePath";
+                    do
+                    {
+                        Console.Write("Choisissez un chemin source pour la sauvegarde : ");
+                        newValue = Console.ReadLine();
+                        if (!Directory.Exists(newValue))
+                        {
+                            Console.WriteLine("Le chemin source n'existe pas. Veuillez entrer un chemin valide.");
+                        }
+                    } while (!Directory.Exists(newValue));
+                    break;
+                }
+                else if (typeInput == "destinationPath")
+                {
+                    property = "destinationPath";
+                    do
+                    {
+                        Console.Write("Choisissez un chemin source pour la sauvegarde : ");
+                        newValue = Console.ReadLine();
+                        if (!Directory.Exists(newValue))
+                        {
+                            Console.WriteLine("Le chemin source n'existe pas. Veuillez entrer un chemin valide.");
+                        }
+                    } while (!Directory.Exists(newValue));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Type de sauvegarde invalide. Veuillez entrer 1 pour Full ou 2 pour Differential.");
+                }
+            }
+            _saveStore.EditSave(id, property, newValue);
+        }
 
 		private void DeleteSave()
 		{
 			_saveStore.DisplayAllSaves();
             Console.WriteLine("Choisissez une sauvegarde à supprimer : ");
             _saveStore.DeleteSave(int.Parse(Console.ReadLine()));
+			Console.Clear();
+            Console.WriteLine("La sauvegarde a été supprimée.");
         }
 
-		private void Quitter()
+		private void Exit()
 		{
 			IsInteracting = false;
 			Console.Clear();
