6b901dc85740d13ba5944d83554f7763c77b2c5a
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-02-12 09:23:40 +0100

    test log window

---
+++ Livrable 2/LogWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace EasySave2._0
+{
+    public partial class LogWindow : Window
+    {
+        public LogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog
+            {
+            };
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                LogFolderPath.Text = folderDialog.FolderName;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
