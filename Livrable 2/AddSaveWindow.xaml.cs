abff040f3a3e1865e67b0e8fb2c15e4184b3ada7
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-02-12 09:33:57 +0100

    AddSaveWindow

---
+++ Livrable 2/AddSaveWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EasySave2._0
+{
+    /// <summary>
+    /// Logique d'interaction pour HomePage.xaml
+    /// </summary>
+    public partial class AddSaveWindow : Window
+    {
+        public AddSaveWindow()
+        {
+            InitializeComponent();
+            DataContext = new HomeViewModel();
+        }
+
+        private void SourceFolder_click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog
+            {
+                // Set options here
+            };
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                SourceFolderBox.Text = folderDialog.FolderName;
+                // Do something with the result
+            }
+        }
+
+        private void DestinationFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new OpenFolderDialog
+            {
+                // Set options here
+            };
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                DestinationFolderBox.Text = folderDialog.FolderName;
+                // Do something with the result
+            }
+        }
+    }
+}
