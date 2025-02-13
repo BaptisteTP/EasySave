178b55093f6a8319862b1c357b937607728b0ffa
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-02-13 11:00:09 +0100

    fix add save

--- Livrable 2/ViewModels/AddSaveViewModel.cs
+++ Livrable 2/ViewModels/AddSaveViewModel.cs
@@ -29,6 +29,7 @@ namespace EasySave2._0.ViewModels
         private void CreateSave()
         {
             saveStore.CreateNewSave(Name, SaveType.Full, SourcePath, DestinationPath);
+
         }
     }
 }
