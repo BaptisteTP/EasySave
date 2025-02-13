2211c22d17bb95ad2e0ddd509c563f76ff31bdc6
TheoC <theo.cvg@gmail.com>
2025-02-13 22:37:11 +0100

    Added files for logging + dll

---
+++ Livrable 2/Models/Logs_Related/Dailylog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EasySave2._0.Models.Logs_Related
+{
+	public class Dailylog
+	{
+		public List<SaveLog> Saves { get; set; } = new List<SaveLog>();
+	}
+
+	public class SaveLog
+	{
+		[XmlAttribute("Save")]
+		public string Savename { get; set; }
+		public List<DirectoryCopyLog> CopiedDirectories { get; set; } = new List<DirectoryCopyLog>();
+		public List<FileCopyLog> CopiedFiles { get; set; } = new List<FileCopyLog>();
+
+		public SaveLog(string savename)
+		{
+			Savename = savename;
+		}
+
+		public SaveLog()
+		{
+
+		}
+	}
+}
