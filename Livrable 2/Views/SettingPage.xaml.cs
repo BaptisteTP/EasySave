921dc170fe5107dbca9ade2669d738a6fbf1026d
DELPECHRomain <romain.delpech1@viacesi.fr>
2025-02-17 13:40:04 +0100

    langues

--- Livrable 2/Views/SettingPage.xaml.cs
+++ Livrable 2/Views/SettingPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EasySave2._0.ViewModels;
 using Microsoft.Win32;
+using System.Globalization;
 
 namespace EasySave2._0
 {
@@ -22,17 +24,31 @@ namespace EasySave2._0
     /// </summary>
     public partial class SettingPage : Page
     {
-        public SettingPage()
+        public SettingPage(SettingsViewModel _settingsViewModel)
         {
             InitializeComponent();
             SettingsViewModel viewModel = new SettingsViewModel();
 			viewModel.SettingsConfirmed += ViewModel_SettingsConfirmed;
             DataContext = viewModel;
         }
-
-		private void ViewModel_SettingsConfirmed(object? sender, EventArgs e)
+        // ----- Methods -----
+        // Select Language in Settings View
+        /*public void updateSelectedLanguage()
+        {
+            if (this.settingsViewModel.model.settings.ActiveLanguage == "en-US")
+            {
+                EnglishButton.BorderBrush = Brushes.DodgerBlue;
+                FrenchButton.BorderBrush = null;
+            }
+            else if (this.settingsViewModel.model.settings.ActiveLanguage == "fr-FR")
+            {
+                EnglishButton.BorderBrush = null;
+                FrenchButton.BorderBrush = Brushes.DodgerBlue;
+            }
+        }*/
+        private void ViewModel_SettingsConfirmed(object? sender, EventArgs e)
 		{
             NavigationService.Navigate(Creator.GetHomePageInstance());
 		}
-	}
+    }
 }
