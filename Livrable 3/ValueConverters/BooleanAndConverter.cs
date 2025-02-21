b14d9d78486481d053252f95c9a9e99a450a00a5
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2025-02-21 12:07:49 +0100

    button cancel whe pause en button pause collapse when the save is pause

---
+++ Livrable 3/ValueConverters/BooleanAndConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace EasySave2._0.ValueConverters
+{
+    public class BooleanAndConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is bool isExecuting && values[1] is bool isPaused)
+            {
+                return isExecuting && !isPaused ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
