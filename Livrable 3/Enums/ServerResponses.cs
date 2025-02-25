407378da08b0f391f27bdead5f0800ec09f5488d
TheoC <theo.cvg@gmail.com>
2025-02-25 19:22:05 +0100

    fixed

--- Livrable 3/Enums/ServerResponses.cs
+++ Livrable 3/Enums/ServerResponses.cs
@@ -20,10 +20,11 @@ namespace EasySave2._0.Enums
 		Save_resumed,
 		Save_stopped,
 
-		Save_already_deleted,
 		Save_already_started,
 		Save_already_paused,
 		Save_already_resumed,
 		Save_already_canceled,
+
+		Cannot_resume_save,
 	}
 }
